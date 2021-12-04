c634ef287be90b4c67b8b9ef4dd79d97a87a8366
Joohy2 <90262714+Joohy2@users.noreply.github.com>
2021-12-04 23:15:25 +0900

    joo1

---
+++ angular-src/src/app/services/validate.service.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ValidateService } from './validate.service';
+
+describe('ValidateService', () => {
+  let service: ValidateService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ValidateService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
+
---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database: "mongodb+srv://joo:12345@cluster0.8oyv4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    secret: 'yoursupersecret',
+    rounds: 10,
+};
